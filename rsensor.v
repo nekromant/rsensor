f950038dd815fbbebc437a1a5b143af648c0ceac
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2013-02-10 01:50:25 +0400

    implement proper random distances
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- rsensor.v
+++ rsensor.v
@@ -2,16 +2,29 @@ module rsensor(
 	       input 	  in_trig,
 	       output reg out_echo
 	       );
-  parameter distance = 50;
+   parameter distance=0;
+   parameter dist_bits=16;
    
   initial begin
      out_echo = 0;
   end
-   integer 			  delay;
+   reg [dist_bits-1:0]			  delay;
    
+   initial begin
+      update_delay;
+   end
    always @( posedge in_trig ) begin
       out_echo <= 0;
-      #distance out_echo <= 1;
+      update_delay;
+      #delay out_echo <= 1;
    end
-   
+
+   task update_delay;
+     begin
+     if (distance == 0)
+	delay <= $random;
+      else
+	delay <= ditance;
+     end
+   endtask
 endmodule
--- tb_rsensor.v
+++ tb_rsensor.v
@@ -2,11 +2,24 @@ module tb_rsensor();
 
    reg trig;
    wire echo;
+
+   /* dist_bits 
+    * distance == 0 : generate a random distance each query
+    * distance == some_number : use a constant distance each call
+    * 
+    * parameter 'dist_bits'
+    * How many bits delay counter should hold. 
+    * This regulates the maximum distance in 
+    * clock cycles. Default value == 16
+    *  
+    */
+   rsensor #(
+	     .distance(0)
+	     ) r (
+		  .in_trig(trig),
+		  .out_echo(echo)
+		  );
    
-   rsensor r(
-	     .in_trig(trig),
-	     .out_echo(echo)
-	     );
    
 	     
    initial begin
@@ -15,6 +28,18 @@ module tb_rsensor();
       $dumpvars(1,echo);
       $monitor("#%d trig:%d echo: %d", $time, trig, echo);
       $display ("Running rsensor testbench");
+      #1 trig = 0;
+      #1 trig = 1;
+      #1 wait(echo==1);
+      $display("sending next");
+      #1 trig = 0;
+      #1 trig = 1;
+      #1 wait(echo==1);
+      $display("sending next");
+      #1 trig = 0;
+      #1 trig = 1;
+      #1 wait(echo==1);
+      $display("sending next");
       #1 trig = 0;
       #1 trig = 1;
       #1 wait(echo==1);
